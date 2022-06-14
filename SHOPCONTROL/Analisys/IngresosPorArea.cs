5e9a9f9377f6fa071f8546c97f9c3a636e13a191
Raul Guerrero <raul.guerrero@linkcode.com.mx>
2022-06-13 21:32:03 -0500

    Added configuration for crystal report printed and change the income report to Unity

--- SHOPCONTROL/Analisys/IngresosPorArea.cs
+++ SHOPCONTROL/Analisys/IngresosPorArea.cs
@@ -23,27 +23,20 @@ namespace SHOPCONTROL.Analisys
             Lv.Columns.Add("Fecha", 80);
             Lv.Columns.Add("Total", 80);
 
-            Lv.Columns.Add("Dental", 80);
+            Lv.Columns.Add("UNIDAD_1", 80);
+            Lv.Columns.Add("UNIDAD_2", 80);
+            Lv.Columns.Add("UNIDAD_3", 80);
+            Lv.Columns.Add("UNIDAD_4", 80);
 
-            Lv.Columns.Add("Endodoncia", 80);
-            Lv.Columns.Add("Farmacia", 80);
-            Lv.Columns.Add("Ginecología", 80);
-            Lv.Columns.Add("Inventario", 80);
-
-            Lv.Columns.Add("Laboratorio", 80);
-            Lv.Columns.Add("Maxilofacial", 80);
-            Lv.Columns.Add("Oftamología", 80);
-            Lv.Columns.Add("Óptica", 80);
-
-
-            Lv.Columns.Add("Ortodoncia", 80);
-            Lv.Columns.Add("Ortopedia-Ortodoncia", 80);
-            Lv.Columns.Add("Papanicolaou", 80);
-            Lv.Columns.Add("Papelería", 80);
-            Lv.Columns.Add("Prótesis", 80);
-            Lv.Columns.Add("Rayos X Dental", 80);
-            Lv.Columns.Add("Sin Categoria", 80);
-            Lv.Columns.Add("Ultrasonido", 80);
+            Lv.Columns.Add("C_ENDONCIA", 100);
+            Lv.Columns.Add("C_GINECOLOGIA", 100);
+            Lv.Columns.Add("C_ULTRASONIDO", 100);
+            Lv.Columns.Add("LABORATORIO", 80);
+            Lv.Columns.Add("OFTALMOLOGIA", 90);
+            Lv.Columns.Add("OPTOMETRIA", 90);
+            Lv.Columns.Add("ORTODONCIA", 90);
+            Lv.Columns.Add("ORTOPEDIA", 90);
+            Lv.Columns.Add("RAYOS_X_DENTAL", 100);
 
 
             Lv.BeginUpdate();
@@ -75,24 +68,19 @@ namespace SHOPCONTROL.Analisys
                 ListViewItem lvi = new ListViewItem(cvcliente);
 
                 lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["TOTAL"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["DENTAL"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["ENDODONCIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["FARMACIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["GINECOLOGIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["INVENTARIO"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["C_ENDONCIA"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["C_GINECOLOGIA"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["C_ULTRASONIDO"].ToString())));
                 lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["LABORATORIO"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["MXILOFACIAL"].ToString())));
                 lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["OFTALMOLOGIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["OPTICA"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["OPTOMETRIA"].ToString())));
                 lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["ORTODONCIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["ORTOPEDIA_ORTODONCIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["PAPANICOLAO"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["PAPELERIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["PROTESIS"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["ORTOPEDIA"].ToString())));
                 lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["RAYOS_X_DENTAL"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["SIN_CATEGORIA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["TIENDA"].ToString())));
-                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["ULTRASONIDO"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["UNIDAD_1"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["UNIDAD_2"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["UNIDAD_3"].ToString())));
+                lvi.SubItems.Add(String.Format("{0:C}", decimal.Parse(leer["UNIDAD_4"].ToString())));
 
 
                 TotalCalculado = TotalCalculado + decimal.Parse(leer["TOTAL"].ToString());
